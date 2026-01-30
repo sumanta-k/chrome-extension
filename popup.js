f71175a8a141ed35d83d7ce2bd865f63286a86c0
Sumanta Kumar Mohanta <mohantasumanta601@gmail.com>
2026-01-30 08:34:16 +0530

    manually setting up icon color to yellow by using 'chrome.action.setIcon'

--- popup.js
+++ popup.js
@@ -1,1 +1,11 @@
 console.log("This is a popup");
+const canvas = new OffscreenCanvas(16, 16);
+const context = canvas.getContext("2d");
+context.clearRect(0, 0, 16, 16);
+context.fillStyle = "#ffFF00"; // yellow
+context.fillRect(0, 0, 16, 16);
+const imageData = context.getImageData(0, 0, 16, 16);
+chrome.action.setIcon({ imageData: imageData }, () => {
+    /* ... */
+});
+
